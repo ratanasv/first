fa7e0390280db615ea91abcef991e8f172a3ed42
Viriya Ratanasangpunth <ratanasv@eecs.oregonstate.edu>
2014-01-26 20:33:45 -0800

    added a carousel of kittens for vistas

--- client/src/app/vistas/vistas.js
+++ client/src/app/vistas/vistas.js
@@ -24,6 +24,20 @@ angular.module( 'ngBoilerplate.vistas', [
 			"And another choice for you.",
 			"but wait! A third!"
 		];
+
+		$scope.carouselInterval = 2000;
+		var slides = $scope.slides = [];
+		$scope.addSlide = function() {
+			var newWidth = 600 + slides.length;
+			slides.push({
+				image: 'http://placekitten.com/' + newWidth + '/300',
+				text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+					['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+			});
+		};
+		for (var i=0; i<4; i++) {
+			$scope.addSlide();
+		}
 	})
 
 ;
