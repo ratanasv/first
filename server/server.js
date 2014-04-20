91309324e920fd294a57c4a0abafb5a661ead75e
Viriya Ratanasangpunth <ratanasv@eecs.oregonstate.edu>
2014-04-20 14:14:32 -0700

    update server.js to use ssl and different auth strategy.

--- server/server.js
+++ server/server.js
@@ -2,53 +2,27 @@
  * Created by ratanasv on 1/25/14.
  */
 
+var fs = require('fs');
+var https = require('https');
 var express = require('express');
 var initWinston = require('./lib/initWinston');
 var passport = require('passport');
-var GoogleStrategy = require('passport-google').Strategy;
-var OAuth2Strategy = require('passport-oauth').OAuth2Strategy;
+var privateKey = fs.readFileSync('~/.ssl/server.key');
+var certificate = fs.readFileSync('~/.ssl/server.crt');
 
 var app = express();
 var winston = initWinston.winston;
 
+var httpsServer = https.createServer({
+    key: privateKey,
+    cert: certificate
+}, app);
 
-
-passport.use(new GoogleStrategy({
-        returnURL: 'http://web.engr.oregonstate.edu/~ratanasv/first/index.html/auth/google/return',
-        realm: 'http://web.engr.oregonstate.edu/~ratanasv'
-    },
-    function(identifier, profile, done) {
-
-        process.nextTick(function () {
-            profile.identifier = identifier;
-            return done(null, profile);
-        });
-    }
-));
-
-app.configure(function() {
-    app.use(express.cookieParser());
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(express.session({ secret: 'keyboard cat' }));
-    // Initialize Passport!  Also use passport.session() middleware, to support
-    // persistent login sessions (recommended).
-    app.use(passport.initialize());
-    app.use(passport.session());
-    app.use(app.router);
-});
-
-app.get('/auth/google', passport.authenticate('google'));
-
-app.get('/auth/google/return',
-    passport.authenticate('google', {
-        successRedirect: '/hello/world',
-        failureRedirect: '/login'
-    }));
 
 app.get('/hello/world', function (req, res){
     res.send('oh hi there');
 });
 
-app.listen(3000);
-winston.info('listening on port 3000...');
+httpsServer.listen(8080);
+
+winston.info('listening on port 8080...');
