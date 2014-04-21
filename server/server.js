dba7e52228c4ad0c2b2ac02aa0a5ed686953c8c9
Viriya Ratanasangpunth <ratanasv@eecs.oregonstate.edu>
2014-04-21 16:14:07 -0700

    use passport local strategy

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var https = require('https');
 var express = require('express');
 var initWinston = require('./lib/initWinston');
 var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 var privateKey = fs.readFileSync('/.ssl/server.key');
 var certificate = fs.readFileSync('/.ssl/server.crt');
 
@@ -18,11 +19,39 @@ var httpsServer = https.createServer({
     cert: certificate
 }, app);
 
+passport.use(new LocalStrategy(
+	function(username, password, done) {
+		console.log(username + ' ' + password);
+		return done(null, username);
+	})
+);
+
+passport.serializeUser(function(user, done) {
+	done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+	done(null, 'asdf');
+});
+
 app.configure(function() {
-	console.log(__dirname);
+	app.use(express.cookieParser());
+	app.use(express.bodyParser());
+	app.use(express.session({
+		secret: 'meow'
+	}));
+	app.use(passport.initialize());
+	app.use(passport.session());
 	app.use('/', express.static(__dirname + '/../client/build'));
 });
 
+app.post('/login',
+	passport.authenticate('local', {
+		successRedirect: '/',
+		failureRedirect: '/login',
+		failureFlash: true
+	})
+);
 
 
 httpsServer.listen(443);
