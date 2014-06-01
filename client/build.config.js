4f91d5b88d83936de7d6795c908cacd34e67c9ea
Viriya Ratanasangpunth <ratanasv@eecs.oregonstate.edu>
2014-06-01 16:57:44 -0700

    Revert "add socket.io dependencies"
    This reverts commit 38a03f3a5328d6ee415a8c9c8e380f3095a47db5.

--- client/build.config.js
+++ client/build.config.js
@@ -69,8 +69,7 @@ module.exports = {
       'vendor/angular-ui-router/release/angular-ui-router.js',
       'vendor/angular-ui-utils/modules/route/route.js',
       'vendor/oauth.io/oauth.min.js',
-      'vendor/d3/d3.js',
-      'vendor/socket.io-client/dist/socket.io.js'
+      'vendor/d3/d3.js'
     ],
     css: [
       'vendor/ionic/css/ionic.css'
