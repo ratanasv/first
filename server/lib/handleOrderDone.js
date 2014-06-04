bfcb5ad6028dedb0dd6b5c0367cae74ae14f05f8
Viriya Ratanasangpunth <ratanasv@eecs.oregonstate.edu>
2014-06-03 18:00:04 -0700

    remove customer, set to done, and publish.

--- server/lib/handleOrderDone.js
+++ server/lib/handleOrderDone.js
@@ -9,10 +9,35 @@ var NO_INFLIGHT_ORDER = require('./redisKeyCompute').NO_INFLIGHT_ORDER;
 var INFLIGHT_ORDERS = require('./redisKeyCompute').INFLIGHT_ORDERS;
 var writeWS = require('./writeWS');
 var async = require('async');
+var BIG_NEGATIVE_DT = -99999999;
 
 module.exports = function(winston) {
-	function setCustomerOrderDone(customer, callback) {
 
+	function removeCustomerFromQueue(customer, callback) {
+		redisClient.lrem(INFLIGHT_ORDERS, 0, computeCustomerKey(customer), function(err, result) {
+			if (err) {
+				callback('cannot delete from inflight list');
+			}
+
+			callback(null, customer);
+		});
+	}
+
+	function setCustomerOrderDone(customer, callback) {
+		redisClient.set(computeCustomerKey(customer), NO_INFLIGHT_ORDER, 'ex', TTL,
+			function(err, result) {
+				if (err) {
+					callback('cannot set customerKey to done');
+				}
+
+				callback(null, customer);
+			}
+		);	
+	}
+
+	function notifyCustomer(customer, callback) {
+		redisClient.publish(computeCustomerKey(customer), BIG_NEGATIVE_DT);
+		callback(null, {});
 	}
 
 	return function(params, ws) {
@@ -21,14 +46,20 @@ module.exports = function(winston) {
 			return writeWS(ws, 400, 'no customer in parameter', {});
 		}
 
-		async.waterfall(
-			[], 
+		async.waterfall([
+			function(callback) {
+				callback(null, customer);
+			}, 
+			removeCustomerFromQueue, 
+			setCustomerOrderDone, 
+			notifyCustomer
+		], 
 			function(err, result) {
 				if (err) {
 					winston.error(err);
 					return writeWS(ws, 500, err, {});
 				}
-				writeWS(ws, 200, '', {});
+				writeWS(ws, 200, '', result);
 			}
 		);
 	};
